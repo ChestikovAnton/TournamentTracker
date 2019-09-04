d6753bb2b7cd2cdbadf1b64608525f18cc199a12
Anton Chestikov <Anton.auc@gmail.com>
2019-09-05 00:17:41 +0500

    Add tournament dashboard form

---
+++ TrackerUI/TournamentDashboard.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TrackerUI
+{
+    partial class TournamentDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadTournamentButton = new System.Windows.Forms.Button();
+            this.loadExistingTournamentDropDown = new System.Windows.Forms.ComboBox();
+            this.loadExistingTournamentLabel = new System.Windows.Forms.Label();
+            this.createTournamentButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loadTournamentButton
+            // 
+            this.loadTournamentButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.loadTournamentButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.loadTournamentButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.loadTournamentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loadTournamentButton.Location = new System.Drawing.Point(30, 117);
+            this.loadTournamentButton.Name = "loadTournamentButton";
+            this.loadTournamentButton.Size = new System.Drawing.Size(377, 48);
+            this.loadTournamentButton.TabIndex = 20;
+            this.loadTournamentButton.Text = "Загрузить турнир";
+            this.loadTournamentButton.UseVisualStyleBackColor = true;
+            // 
+            // loadExistingTournamentDropDown
+            // 
+            this.loadExistingTournamentDropDown.FormattingEnabled = true;
+            this.loadExistingTournamentDropDown.Location = new System.Drawing.Point(30, 68);
+            this.loadExistingTournamentDropDown.Name = "loadExistingTournamentDropDown";
+            this.loadExistingTournamentDropDown.Size = new System.Drawing.Size(377, 38);
+            this.loadExistingTournamentDropDown.TabIndex = 19;
+            // 
+            // loadExistingTournamentLabel
+            // 
+            this.loadExistingTournamentLabel.AutoSize = true;
+            this.loadExistingTournamentLabel.Location = new System.Drawing.Point(30, 35);
+            this.loadExistingTournamentLabel.Name = "loadExistingTournamentLabel";
+            this.loadExistingTournamentLabel.Size = new System.Drawing.Size(286, 30);
+            this.loadExistingTournamentLabel.TabIndex = 18;
+            this.loadExistingTournamentLabel.Text = "Выбор турнира для загрузки";
+            // 
+            // createTournamentButton
+            // 
+            this.createTournamentButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.createTournamentButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.createTournamentButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.createTournamentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createTournamentButton.Location = new System.Drawing.Point(30, 218);
+            this.createTournamentButton.Name = "createTournamentButton";
+            this.createTournamentButton.Size = new System.Drawing.Size(377, 48);
+            this.createTournamentButton.TabIndex = 20;
+            this.createTournamentButton.Text = "Создать новый турнир";
+            this.createTournamentButton.UseVisualStyleBackColor = true;
+            // 
+            // TournamentDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(438, 296);
+            this.Controls.Add(this.createTournamentButton);
+            this.Controls.Add(this.loadTournamentButton);
+            this.Controls.Add(this.loadExistingTournamentDropDown);
+            this.Controls.Add(this.loadExistingTournamentLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "TournamentDashboard";
+            this.Text = "Турниры";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadTournamentButton;
+        private System.Windows.Forms.ComboBox loadExistingTournamentDropDown;
+        private System.Windows.Forms.Label loadExistingTournamentLabel;
+        private System.Windows.Forms.Button createTournamentButton;
+    }
+}
