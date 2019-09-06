ac92a7c181a3bd2103a1e2f0be24d981f14c8fd3
Anton Chestikov <Anton.auc@gmail.com>
2019-09-06 22:18:48 +0500

    Add save prizes to file

--- TrackerLibrary/DataAccess/TextFilesConnector.cs
+++ TrackerLibrary/DataAccess/TextFilesConnector.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextFilesHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextFilesConnector : IDataConnection
     {
-        // TODO - Wire up the CreatePrize for text file.
+        private const string PrizesFileName = "PrizeModel.csv";
+
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            List<PrizeModel> prizes = PrizesFileName.GetFullFilePath().LoadFileOrGetNewStringList().ConvertToPrizeModel();
+
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            
+            model.Id = currentId;
+
+            prizes.Add(model);
+
+            prizes.SaveToPrizeFile(PrizesFileName);
 
             return model;
         }
